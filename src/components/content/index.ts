6e29582b8571c817f7635f4d52f91bf62c393aec
jpinfinite <j.p.designgraficoinfinite@gmail.com>
2025-11-24 12:58:47 -0300

    fix: corrigir imports faltantes e remover arquivos problematicos
    - Removido arquivos de atualizacao que causavam erro de build
    - Adicionado todos os componentes faltantes ao index.ts
    - ArticleGrid, FeaturedArticleCard, ArticleLayout agora exportados
    - Todos os componentes de content/ agora disponiveis
    - ESLint passando sem erros

--- src/components/content/index.ts
+++ src/components/content/index.ts
@@ -1,4 +1,6 @@
 // Exportar todos os componentes de conteúdo
+
+// Novos componentes criados
 export { ExchangeAffiliateLinks } from './ExchangeAffiliateLinks'
 export { InfoBox } from './InfoBox'
 export { ComparisonTable } from './ComparisonTable'
@@ -6,3 +8,17 @@ export { FAQ } from './FAQ'
 export { DisclaimerBox } from './DisclaimerBox'
 export { TableOfContents } from './TableOfContents'
 export { RelatedArticles } from './RelatedArticles'
+
+// Componentes existentes
+export { ArticleGrid } from './ArticleGrid'
+export { FeaturedArticleCard } from './FeaturedArticleCard'
+export { ArticleLayout } from './ArticleLayout'
+export { ArticleCard } from './ArticleCard'
+export { ArticleFilters } from './ArticleFilters'
+export { AlertBox } from './AlertBox'
+export { CTAExamples } from './CTAExamples'
+export { MetaMaskArticle } from './MetaMaskArticle'
+export { NewsletterForm } from './NewsletterForm'
+export { RelatedArticlesInline } from './RelatedArticlesInline'
+export { TipBox } from './TipBox'
+export { WarningBox } from './WarningBox'
